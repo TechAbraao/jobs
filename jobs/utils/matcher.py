d92ceee2c0a751a772ef114802ca5c56b1028e11
TechAbraao <abraao.cutielips@hotmail.com>
2026-07-21 16:19:24 -0300

    fix: adicionando comentário importante

--- jobs/utils/matcher.py
+++ jobs/utils/matcher.py
@@ -1,4 +1,5 @@
 def calculate_match(job: dict, keywords: list[str]):
+    
     text = (
         job["name"]
         + "\n"
@@ -10,7 +11,7 @@ def calculate_match(job: dict, keywords: list[str]):
     for keyword in keywords:
         if keyword.lower() in text:
             found.append(keyword)
-
+    # o score é a quantidade de palavras-chave encontradas ÷ quantidade total de palavras-chave × 100 (pra da em porcentagem)
     score = (len(found) / len(keywords) * 100) if keywords else 0
 
     return score, found
